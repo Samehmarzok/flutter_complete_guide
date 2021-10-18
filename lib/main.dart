a5fe18352c5c79e8f09a145799e1d61d745f4d3c
Samehmarzok@github.com <sameh.marzok@gmail.com>
2021-10-18 10:00:32 +0200

    Custom Widgets & New Classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,54 @@
+import 'package:flutter/material.dart';
 
+import './question.dart';
+import './answer.dart';
+
+//void main() {
+//runApp(MyApp());
+//}
+// Shorter Syntex for void
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestions() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorit color?',
+      'What\'s your favorit Animal?'
+    ];
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+      ),
+      body: Column(
+        children: [
+          // Custom Widget File
+          Question(
+            questions[_questionIndex],
+          ),
+          Answer(_answerQuestions),
+          Answer(_answerQuestions),
+          Answer(_answerQuestions),
+        ],
+      ),
+    ));
+  }
+}
