80af02e72c5d826e2780e0f372afc58baeec84c8
Samehmarzok <sameh.marzok@example.com>
2021-10-20 12:35:17 +0200

    Const VS Final Update

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
+    const questions = [
       //Create a Map
       {
         'QuestionText': 'What\'s your favorit color?',
